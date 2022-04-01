6a2b4fc9a958ce6c3aa5fad40d2c567b198e690d
sevimozsoy <sevimozsoy30@gmail.com>
2022-04-01 23:35:23 +0300

    skeleton-placeholder

---
+++ src/Skeleton-placeholder.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import  SkeletonPlaceholder  from "react-native-skeleton-placeholder";
+
+const Skeleton = ({ }) => (
+    <SkeletonPlaceholder>
+        <SkeletonPlaceholder.Item
+            width={100}
+            height={100}
+            borderRadius={100}
+            borderWidth={5}
+            borderColor="white"
+            alignSelf="center"
+            position="relative"
+        />
+        <SkeletonPlaceholder.Item
+            width={120}
+            height={20}
+            alignSelf="center"
+            marginTop={12}
+            borderRadius={4}
+        />
+        <SkeletonPlaceholder.Item
+            width={240}
+            height={20}
+            alignSelf="center"
+            marginTop={12}
+            borderRadius={4}
+        />
+        <SkeletonPlaceholder.Item
+            width={240}
+            height={20}
+            alignSelf="center"
+            marginTop={12}
+            borderRadius={4}
+        />
+    </SkeletonPlaceholder>
+);
+
+export default Skeleton;
